cf64fadc3b5aa3f35d2f1b4caad56cf3b1ec04d4
Risnan Rv <risnanrv.1877@gmail.com>
2025-04-09 16:30:07 +0530

    About Page Added

--- src/Components/AboutText/AboutText.jsx
+++ src/Components/AboutText/AboutText.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import './AboutText.css'
 function AboutText() {
   return (
-    <div>
-      
-    </div>
+   <div className="about-text">
+     <div className="text-center mb-12">
+        <h1 className="text-3xl font-bold text-[#4328c7] section-title">About WebZin</h1>
+        </div>
+        <div className="about-paragraph-container">
+        <p className="about-paragraph max-w-3xl mx-auto text-lg text-gray-800 leading-relaxed text-left">
+  At WebZin, we bring your vision to life through powerful digital experiences. As a passionate team of freelance developers, we craft modern, responsive, and high-performance websites tailored to your needs. Whether it's a personal portfolio or a full-scale business site, our mission remains the same: Your Vision, Our Code. We prioritize clean design, efficient code, and your satisfaction at every step. From building your brand from scratch to scaling your digital presence, WebZin is your trusted tech partner.
+</p>
+
+        </div>
+    
+   </div>
   )
 }
 
